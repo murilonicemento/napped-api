bf921a15adaa5833eb0ad1eed311c0aa44266ed8
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-24 20:41:10 -0300

    teste de conexão com db e rotas

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use App\Models\User;
+use App\Controllers\UserController;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -11,12 +13,16 @@ $dotenv->load();
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-  $data = ["nome" => "Murilo", "idade" => 20];
-  $payload = json_encode($data);
-  $response->getBody()->write($payload);
 
-  return $response->withHeader("Content-Type", "application/json")->withStatus(200);
+$app->get('/', function (Request $request, Response $response) {
+  $users = new User();
+  $data = json_encode($users->getUser());
+
+  $response->getBody()->write($data);
+  return $response
+    ->withHeader('Content-Type', 'application/json')
+    ->withStatus(201);
+  return $response->getBody()->write($users->getUser());
 });
 
 $app->run();
