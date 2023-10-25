25d5f9fe1d8a4a28fa15ff956314d445d706dab0
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-25 18:26:51 -0300

    configuração do index para utilização das rotas

--- public/index.php
+++ public/index.php
@@ -1,10 +1,6 @@
 <?php
 
 use Slim\Factory\AppFactory;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-use App\Models\User;
-use App\Controllers\UserController;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -13,16 +9,7 @@ $dotenv->load();
 
 $app = AppFactory::create();
 
-
-$app->get('/', function (Request $request, Response $response) {
-  $users = new User();
-  $data = json_encode($users->getUser());
-
-  $response->getBody()->write($data);
-  return $response
-    ->withHeader('Content-Type', 'application/json')
-    ->withStatus(201);
-  return $response->getBody()->write($users->getUser());
-});
+$routes = require __DIR__ . "/../app/Routes.php";
+$routes($app);
 
 $app->run();
