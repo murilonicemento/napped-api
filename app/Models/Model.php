de3f2a7b37b99788114d2ac685d9651c40a115b0
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-27 19:31:16 -0300

    feat: criação de classe base para os Models

---
+++ app/Models/Model.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Connection;
+
+class Model extends Connection {
+
+  protected $connection;
+
+  public function __construct() {
+    $this->connection = $this->DB();
+  }
+}
