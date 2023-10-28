ef32cb14601fe826432add378db1f47d4aeaff81
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-28 12:22:34 -0300

    feat(login): criação de rota de login

--- app/Routes.php
+++ app/Routes.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use App\Controllers\UserController;
 
 return function (App $app) {
-  $app->post('/api/register', function (Request $request, Response $response) {
+  $app->post("/api/register", function (Request $request, Response $response) {
     try {
       $data = $request->getParsedBody();
       $payload = UserController::registerUser($data["name"], $data["email"], $data["password"]);
@@ -19,6 +19,23 @@ return function (App $app) {
       return $response->withHeader("Content-Type", "application/json")->withStatus($statusCode);
     } catch (\Exception $exception) {
       $response->getBody()->write(json_encode(["error" => "Erro ao tentar criar usuário"]));
+
+      return $response->withHeader("Content-Type", "application/json")->withStatus(500);
+    }
+  });
+
+  $app->post("/api/login", function (Request $request, Response $response) {
+    try {
+      $data = $request->getParsedBody();
+
+      $payload = UserController::loginUser($data["email"], $data["password"]);
+      $statusCode = $payload["statusCode"];
+
+      $response->getBody()->write(json_encode($payload));
+
+      return $response->withHeader("Content-Type", "application/json")->withStatus($statusCode);
+    } catch (\Exception $exception) {
+      $response->getBody()->write(json_encode(["error" => "Erro ao realizar login"]));
 
       return $response->withHeader("Content-Type", "application/json")->withStatus(500);
     }
