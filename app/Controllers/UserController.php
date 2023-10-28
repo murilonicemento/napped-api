533ef0d48f883fcd8981ad3d3468718cbaa6603e
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-28 12:23:43 -0300

    feat(controller): criação de lógica para login do usuário

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -13,9 +13,28 @@ class UserController {
 
       if (!$newUser) return ["error" => "Usuário já cadastrado", "statusCode" => 400];
 
+      $newUser["statusCode"] = 200;
+
       return $newUser;
     } catch (\Exception $exception) {
-      return $exception->getMessage();
+      throw $exception->getMessage();
+    }
+  }
+
+  public static function loginUser($email, $password) {
+    try {
+      $user = new User();
+      $data = $user->login($email);
+
+      if (!password_verify($password, $data["password"])) return ["error" => "Usuário ou senha inválidos", "statusCode" => 400];
+
+      if (!$data) return ["error" => "Usuário não cadastrado", "statusCode" => 400];
+
+      $data["statusCode"] = 200;
+
+      return $data;
+    } catch (\Exception $exception) {
+      throw $exception->getMessage();
     }
   }
 }
