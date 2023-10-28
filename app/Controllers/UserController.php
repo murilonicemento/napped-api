b6e2f7939b233da3e46cb2609e8ae84787940ee8
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-28 12:40:18 -0300

    feat(controller): criação de lógica para deletar usuário

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController {
 
       $newUser = $user->register($email, $name, password_hash($password, PASSWORD_DEFAULT));
 
-      if (!$newUser) return ["error" => "Usuário já cadastrado", "statusCode" => 400];
+      if (!$newUser) return ["error" => "Usuário já cadastrado.", "statusCode" => 400];
 
       $newUser["statusCode"] = 200;
 
@@ -26,13 +26,24 @@ class UserController {
       $user = new User();
       $data = $user->login($email);
 
-      if (!password_verify($password, $data["password"])) return ["error" => "Usuário ou senha inválidos", "statusCode" => 400];
+      if (!password_verify($password, $data["password"])) return ["error" => "Usuário ou senha inválidos.", "statusCode" => 400];
 
-      if (!$data) return ["error" => "Usuário não cadastrado", "statusCode" => 400];
+      if (!$data) return ["error" => "Usuário não cadastrado.", "statusCode" => 400];
 
       $data["statusCode"] = 200;
 
       return $data;
+    } catch (\Exception $exception) {
+      throw $exception->getMessage();
+    }
+  }
+
+  public static function deleteUser($id) {
+    try {
+      $user = new User();
+      $data = $user->delete($id);
+
+      return $data ? ["message" => "Usuário deletado com sucesso."] : ["message" => "Erro ao tentar deletar usuário."];
     } catch (\Exception $exception) {
       throw $exception->getMessage();
     }
