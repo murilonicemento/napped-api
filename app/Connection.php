6ff19c965ea461612edde734465ea0753dbe4745
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-13 18:34:34 -0300

    feat: inclusão de trativas de erro

--- app/Connection.php
+++ app/Connection.php
@@ -5,7 +5,11 @@ namespace App;
 abstract class Connection {
   public function DB() {
     try {
-      $connection = new \PDO("mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_DATABASE']};charset=utf8", $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"]);
+      $options_pdo = [
+        \PDO::ATTR_ERRMODE =>  \PDO::ERRMODE_EXCEPTION
+      ];
+
+      $connection = new \PDO("mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_DATABASE']};charset=utf8", $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $options_pdo);
 
       return $connection;
     } catch (\PDOException $exception) {
