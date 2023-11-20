55f34cfe30e8882458c8598624543caa4620f0b3
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-20 19:38:11 -0300

    feat: criação de lógica para envio de e-mail ao realizar o registro de usuário

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Controllers;
 
 use App\Models\Auth;
 use Firebase\JWT\JWT;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class AuthController {
   public static function registerUser($email, $name, $password) {
@@ -49,6 +52,47 @@ class AuthController {
     $token = $auth->validateToken($id);
 
     return empty($token) || $userToken !== $token ? ["error" => ["message" => "Token inválido."], "statusCode" => 401] : true;
+  }
+
+  public static function sendMail($email, $name) {
+    $mail = new PHPMailer(true);
+
+    try {
+      //Server settings
+      $mail->SMTPDebug = SMTP::DEBUG_SERVER;                //Enable verbose debug output
+      $mail->isSMTP();                                      //Send using SMTP
+      $mail->Host       = "smtp.example.com";               //Set the SMTP server to send through
+      $mail->SMTPAuth   = true;                             //Enable SMTP authentication
+      $mail->Username   = "user@example.com";               //SMTP username
+      $mail->Password   = "secret";                         //SMTP password
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;      //Enable implicit TLS encryption
+      $mail->Port       = 465;                              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+      //Recipients
+      $mail->setFrom("napped@gmail.com", "Napped");
+      $mail->addAddress($email, $name);                     //Add a recipient
+
+      //Attachments
+      $mail->addAttachment("/var/tmp/file.tar.gz");         //Add attachments
+      $mail->addAttachment("/tmp/image.jpg", "new.jpg");    //Optional name
+
+      //Content
+      $mail->isHTML(true);                                  //Set email format to HTML
+      $mail->Subject = "Bem-vindo ao Napped, $name!";
+      $mail->Body    = "
+        <p>Olá, $name</p>
+        <p>Bem-vindo ao Napped, sua fonte de informações sobre o mundo nerd. Aqui você encontrará detalhes sobre séries, filmes, animes e jogos.</p>
+        <p>Continue explorando e aproveitando o melhor do universo geek!</p>
+        <p>Atenciosamente,<br>Equipe Napped!</p>
+      ";
+      $mail->AltBody = "Bem-vindo ao Projeto Napped! O Napped é uma fonte de informações sobre o mundo nerd, abrangendo séries, filmes, animes e jogos. Continue explorando e aproveitando o melhor do universo geek! Atenciosamente, Equipe Napped!";
+
+      $mail->send();
+
+      return ["message" => "E-mail enviado com sucesso."];
+    } catch (Exception $e) {
+      return ["message" => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"];
+    }
   }
 
   public static function loginUser($email, $password) {
