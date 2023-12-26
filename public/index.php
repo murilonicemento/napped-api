59628e3f1afcfc9c9b559d15018aad851e5ed8ae
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-26 19:15:17 -0300

    chore: inclusão ao CORS rota

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ $app->add(function ($request, $handler) {
     ->withHeader("Content-Type", "application/json")
     ->withHeader("Access-Control-Allow-Origin", "http://localhost:5173")
     ->withHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-    ->withHeader("Access-Control-Allow-Methods", "POST, DELETE, OPTIONS");
+    ->withHeader("Access-Control-Allow-Methods", "GET", "POST, DELETE, OPTIONS");
 });
 
 $userRoutes = require __DIR__ . "/../app/Routes/userRoutes.php";
